4212e9786095bd0982ee79497ce98f2f4d78835c
Tran Vinh Khai <vkhai.tran@gmail.com>
2023-01-30 00:00:10 +0700

    load uncompressed texture

--- BuildData.bat
+++ BuildData.bat
@@ -4,4 +4,6 @@ if not exist .\release (
 	mkdir release
 )
 
-compressonatorcli -fd BC1 .\data\OK!.png .\release\OK!
+compressonatorcli -fd BC7 .\data\OK!.png .\release\OK!
+xcopy .\data\*.vs .\release
+xcopy .\data\*.fs .\release
